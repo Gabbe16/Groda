0674d1972b23c8c9b78aafd723bc574cd0c87e72
Gabbe16 <94972340+Gabbe16@users.noreply.github.com>
2023-12-07 13:24:28 +0100

    Fler instanser

--- src/Groda.java
+++ src/Groda.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Groda {
+public class Groda {
+    int height;
+    int width;
+    int weight;
+    int color;
+
+    public Groda(int height, int width, int weight, int color) {
+        this.height = height;
+        this.width = width;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public void Jump(){
+
+    }
+
+    public void Eat(){
+        System.out.println("Nom Nom");
+    }
+
+    public void Walk(){
+
+    }
+
+    public void Croack(){
+        System.out.println("Croak");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Groda grodis = new Groda(10,5,20, 0x008000);
+        grodis.Croack();
+        Groda jens = new Groda(20,10,40, 0xFFFF00);
+        jens.Eat();
     }
 }
